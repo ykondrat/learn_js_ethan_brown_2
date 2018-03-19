9e79c17d7bc5b8e2a91e9d0cd6d6731ea4485662
Yevhen Kondratyev <kondratyev.yevhen@gmail.com>
2018-03-20 00:16:21 +0200

    end of capter

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function() {
+    // Run eslint
+    gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format());
+    // Code for Node
+    gulp.src('es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+    // Public Code
+    gulp.src('public/es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('public/dist'));
+});
